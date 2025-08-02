a881835476e33fe7941a02abc82aa2307f37add0
Hunain Shaikh <shaikhhunain12@gmail.com>
2025-08-03 00:05:41 +0530

    Create Task17.java

---
+++ Task17.java
@@ -0,0 +1,18 @@
+//Largest value finder
+//Written by Hunain Shaikh 241P043
+
+package main.java.com.example;
+
+public class Task17 {
+    public static void main(String[] args) {
+        int[] arr = {67, 2, 3, 405, 5, 6, 7};
+        int big = arr[0], n = arr.length;
+        for(int i = 0; i < n; i++){
+            if(big < arr[i]){
+                big = arr[i];
+            }
+            else continue;
+        }
+        System.out.printf("Largest number in array is %d", big);
+    }
+}
